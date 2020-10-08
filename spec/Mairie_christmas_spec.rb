3a48b4830ca93db3b097f3b9ad8508c6e84eec3c
GRDTHP <71724930+GRDTHP@users.noreply.github.com>
2020-10-09 01:30:15 +0200

    Update Mairie_christmas_spec.rb

--- spec/Mairie_christmas_spec.rb
+++ spec/Mairie_christmas_spec.rb
@@ -4,5 +4,5 @@ begin
     get_townhall_url_scrapper
 
     rescue => e
-    puts "l'extraction des données n'est plus possible" #résultat que tu veux voir en lieu et place d'une erreur terminal
+    puts "l'extraction des données n'est plus possible" 
 end
